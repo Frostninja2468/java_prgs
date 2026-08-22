9d794789a34e083ac3bb0491a8c1132d533edc8b
Aaron Chaudhuri <aaron.chaudhuri19@gmail.com>
2026-08-22 11:06:26 +0530

    Add files via upload

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -1,1 +1,13 @@
+import java.util.*;
+public class Hangman
+{
+    static Scanner s = new Scanner(System.in);
+    static char[] alph = new char[26];
+    static String[] hman = {""};
 
+    public static void main(String args[])
+    {
+        
+    }
+
+}
